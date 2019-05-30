560c32adc3beccc2238e51ddcb3d7eff0f9e4a09
Elixir Kook <justkook@gmail.com>
2019-05-31 02:49:30 +0900

    added router-link for home, gallery, map, contact

---
+++ src/views/Contact.vue
@@ -0,0 +1,7 @@
+
+<template>
+  <div class="contact">
+    <router-link to="/">back</router-link>
+    <h1>Contact</h1>
+  </div>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,81 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="w-100">
+      <img class="bg-img" src="./../assets/ours/eysp.jpeg">
+    </div>
+    <div class="w-100" style="position: absolute; top:0px; left:0px">
+      <div class="p-5 m-5">
+      </div>
+      <div class="p-5">
+        <div class="title d-font font-weight-bold p-2">박은영 ❤ 국성표</div>
+        <div class="p-5 font-weight-bold">
+          <div class="d-font">2019. 8. 31 (토) 낮 12시</div>
+          <div class="d-font">신도림 라마다 14층</div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+  name: 'app',
 }
 </script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.bg-kookey {
+  background-color: #FFFFFF;
+}
+
+.marry-title {
+  margin-bottom: -10px !important;
+  z-index: 100 !important;
+;
+}
+
+.img-content {
+  width: 100%;
+} 
+
+.bg-img {
+  width: 100%;
+  z-index: -1;
+  position: 50%;
+  opacity: .5;
+}
+
+.nav-icon {
+  width: 40px;
+  height: 40px;
+}
+
+.wd-color {
+    color: #328f95;
+}
+
+@font-face { font-family: 'Daehan'; src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_one@1.0/Daehan.woff') format('woff'); font-weight: normal; font-style: normal; }
+
+.d-font {
+    /*color: #757575;*/
+    font-family: Daehan, Helvetica;
+    font-weight: bold;
+    text-rendering: optimizeLegibility;
+    word-break: keep-all;
+}
+
+.title {
+  font-size: 32px;
+
+}
+
+</style>
