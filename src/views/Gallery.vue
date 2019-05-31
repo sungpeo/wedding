8323ebcdf52a8c0f2c00d1591922755957b1bdbd
Elixir Kook <justkook@gmail.com>
2019-05-31 23:53:08 +0900

    created thumbnails and added title images, and so on

--- src/views/Gallery.vue
+++ src/views/Gallery.vue
@@ -1,7 +1,43 @@
 
 <template>
   <div class="gallery">
-    <router-link to="/">back</router-link>
-    <h1>Gallery</h1>
+    <img class="sub-title" src="../assets/gallery-title.png">
+
+    <div class="photobook">
+      <div class="photoblock" align="left">
+        <img class="photo" src="../assets/ours/thumbnails/selected-1-sm.jpg">
+        <img class="photo" src="../assets/ours/thumbnails/selected-2-sm.jpg">
+        <img class="photo" src="../assets/ours/thumbnails/selected-3-sm.jpg">
+        <img class="photo" src="../assets/ours/thumbnails/selected-4-sm.jpg">
+      </div>
+      <div class="photoblock" align="left">
+        <img class="photo" src="../assets/ours/thumbnails/selected-5-sm.jpg">
+        <img class="photo" src="../assets/ours/thumbnails/selected-6-sm.jpg">
+        <img class="photo" src="../assets/ours/thumbnails/selected-7-sm.jpg">
+        <img class="photo" src="../assets/ours/thumbnails/selected-8-sm.jpg">
+      </div>
+      <div class="photoblock" align="left">
+        <img class="photo" src="../assets/ours/thumbnails/selected-9-sm.jpg">
+        <img class="photo" src="../assets/ours/thumbnails/selected-10-sm.jpg">
+        <img class="photo" src="../assets/ours/thumbnails/selected-11-sm.jpg">
+        <img class="photo" src="../assets/ours/thumbnails/selected-12-sm.jpg">
+      </div>
+      <div class="photoblock" align="left">
+        <img class="photo" src="../assets/ours/thumbnails/selected-13-sm.jpg">
+      </div>
+    </div>
   </div>
 </template>
+
+
+<style scoped>
+
+.photobook {
+  width: 100%;
+}
+.photo {
+  width: 25%;
+  padding: 2px;
+}
+
+</style>
