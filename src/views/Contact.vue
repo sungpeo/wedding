0287158184aea09d840d326f41e1b4ae2f67aded
Elixir Kook <justkook@gmail.com>
2019-06-01 02:04:48 +0900

    added basic information of family

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -3,31 +3,48 @@
   <div class="contact">
     <img class="sub-title" src="../assets/contact-title.png">
 
-    <div>
+    <div class="p-2">
       <div>함께 했던 시간들 속에서</div>
       <div>우리는 서로를 더 좋은 사람으로 만들어 주었습니다.</div>
       <div>매일이 젊은 날처럼 반짝거릴 순 없겠지만</div>
-      <div>지금과 같은 결의 마음으로 오래도록 사랑하며 살아가겠습니다.</div>
-      <div>이제 소중한 분들을 모시고 그 마음을 세상 앞에 약속하고자 합니다.</div>
+      <div>지금과 같은 결의 마음으로 <br>오래도록 사랑하며 살아가겠습니다.</div>
+      <div>이제 소중한 분들을 모시고 <br>그 마음을 세상 앞에 약속하고자 합니다.</div>
       <div>바쁘시더라도 부디 오셔서 저희의 앞날을 축복해 주시고</div>
       <div>격려해 주시면 더없는 기쁨이 되겠습니다.</div>
     </div>
 
-    <div>신랑 국성표</div>
-    <div>신부 박은영</div>
-    <div>
-      <div>신랑 측 혼주</div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+    <div class="p-2">
+      <div>
+        <div>신랑 국성표</div>
+        <div style="justify-content:space-around;">
+          <div>call</div>
+          <div>message</div>
+        </div>
+      </div>
+      <div>
+        <div>신부 박은영</div>
+        <div style="justify-content:space-around;">
+          <div>call</div>
+          <div>message</div>
+        </div>
+      </div>
     </div>
+
     <div>
-      <div>신부 측 혼주</div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+      <div class="p-2 pl-5 float-left">
+        <div>신랑 측 혼주</div>
+        <div>아버지 국경만</div>
+        <div></div>
+        <div>어머니 안복순</div>
+        <div></div>
+      </div>
+      <div class="p-2 pr-5 float-right">
+        <div>신부 측 혼주</div>
+        <div>아버지 박용덕</div>
+        <div></div>
+        <div>어머니 김정숙</div>
+        <div></div>
+      </div>
     </div>
   </div>
 </template>
