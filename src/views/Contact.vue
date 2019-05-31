af86905573c4b7b77b6deb6dd05db9b1602d90da
Elixir Kook <justkook@gmail.com>
2019-06-01 03:24:23 +0900

    added call, message buttons

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -2,6 +2,7 @@
 <template>
   <div class="contact">
     <img class="sub-title" src="../assets/contact-title.png">
+    <!--<h1>Invitation</h1>-->
 
     <div class="p-2">
       <div>함께 했던 시간들 속에서</div>
@@ -13,38 +14,121 @@
       <div>격려해 주시면 더없는 기쁨이 되겠습니다.</div>
     </div>
 
-    <div class="p-2">
-      <div>
-        <div>신랑 국성표</div>
-        <div style="justify-content:space-around;">
-          <div>call</div>
-          <div>message</div>
+    <div class="p-4">
+      <div class="w-100 p-1">
+        <div class="w-50 d-inline-block">신랑 국성표</div>
+        <div class="w-50 d-inline-block">
+          <div class="p-1 d-inline-block">
+            <a href="tel:010-5312-6382">
+              <img class="call-img" src="../assets/call.png">
+            </a>
+          </div>
+          <div class="pl-4 d-inline-block">
+            <a href="sms:010-5312-6382">
+              <img class="msg-img" src="../assets/message.png">
+            </a>
+          </div>
         </div>
       </div>
-      <div>
-        <div>신부 박은영</div>
-        <div style="justify-content:space-around;">
-          <div>call</div>
-          <div>message</div>
+      <div class="w-100 p-1">
+        <div class="w-50 d-inline-block">신부 박은영</div>
+        <div class="w-50 d-inline-block">
+          <div class="p-1 d-inline-block">
+            <a href="tel:010-4324-9095">
+              <img class="call-img" src="../assets/call.png">
+            </a>
+          </div>
+          <div class="pl-4 d-inline-block">
+            <a href="sms:010-4324-9095">
+              <img class="msg-img" src="../assets/message.png">
+            </a>
+          </div>
         </div>
       </div>
     </div>
 
-    <div>
-      <div class="p-2 pl-5 float-left">
-        <div>신랑 측 혼주</div>
+    <div class="w-100 pb-5 mb-5">
+
+      <div class="w-50 d-inline-block">
+
+        <div class="p-3">신랑 측 혼주</div>
         <div>아버지 국경만</div>
-        <div></div>
-        <div>어머니 안복순</div>
-        <div></div>
+        <div class="p-2">
+          <div class="m-1 d-inline-block">
+            <a href="tel:010-6266-6382">
+              <img class="call-img-sm" src="../assets/call.png">
+            </a>
+          </div>
+          <div class="m-1 pl-3 d-inline-block">
+            <a href="sms:010-6266-6382">
+              <img class="msg-img-sm" src="../assets/message.png">
+            </a>
+          </div>
+        </div>
+
+        <div class="pt-3">어머니 안복순</div>
+        <div class="p-2">
+          <div class="m-1 d-inline-block">
+            <a href="tel:010-5737-6382">
+              <img class="call-img-sm" src="../assets/call.png">
+            </a>
+          </div>
+          <div class="m-1 pl-3 d-inline-block">
+            <a href="sms:010-5737-6382">
+              <img class="msg-img-sm" src="../assets/message.png">
+            </a>
+          </div>
+        </div>
+
       </div>
-      <div class="p-2 pr-5 float-right">
-        <div>신부 측 혼주</div>
+
+      <div class="w-50 d-inline-block">
+        <div class="p-3">신부 측 혼주</div>
         <div>아버지 박용덕</div>
-        <div></div>
-        <div>어머니 김정숙</div>
-        <div></div>
+        <div class="p-2">
+          <div class="m-1 d-inline-block">
+            <a href="tel:010-">
+              <img class="call-img-sm" src="../assets/call.png">
+            </a>
+          </div>
+          <div class="m-1 pl-3 d-inline-block">
+            <a href="sms:010-">
+              <img class="msg-img-sm" src="../assets/message.png">
+            </a>
+          </div>
+        </div>
+        <div class="pt-3">어머니 김정숙</div>
+        <div class="p-2">
+          <div class="m-1 d-inline-block">
+            <a href="tel:010-5015-9223">
+              <img class="call-img-sm" src="../assets/call.png">
+            </a>
+          </div>
+          <div class="m-1 pl-3 d-inline-block">
+            <a href="sms:010-5015-9223">
+              <img class="msg-img-sm" src="../assets/message.png">
+            </a>
+          </div>
+        </div>
       </div>
+
     </div>
   </div>
 </template>
+
+<style scoped>
+.call-img {
+  width: 35px;
+  height: 35px;
+}
+.msg-img {
+  height: 35px
+}
+.call-img-sm {
+  width: 30px;
+  height: 30px;
+}
+.msg-img-sm {
+  height: 30px
+}
+</style>
